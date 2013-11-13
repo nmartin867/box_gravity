d02df559e8a1c22b2755957d0dddd1acd3ad57be
nmartin <nmartin867@gmail.com>
2013-11-12 21:05:08 -0700

    init checkin

---
+++ box_gravity/GameLayer.mm
@@ -0,0 +1,264 @@
+//
+//  HelloWorldLayer.mm
+//  box_gravity
+//
+//  Created by Nick Martin on 11/11/13.
+//  Copyright Nick Martin 2013. All rights reserved.
+//
+
+// Import the interfaces
+#import "GameLayer.h"
+
+// Not included in "cocos2d.h"
+#import "CCPhysicsSprite.h"
+
+// Needed to obtain the Navigation Controller
+#import "AppDelegate.h"
+
+
+
+
+enum {
+	kTagParentNode = 1,
+};
+
+
+#pragma mark - HelloWorldLayer
+
+@interface GameLayer()
+-(void) initPhysics;
+-(void) addNewSpriteAtPosition:(CGPoint)p;
+@end
+
+@implementation GameLayer
+
++(CCScene *) scene
+{
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	GameLayer *layer = [GameLayer node];
+	
+	// add layer as a child to scene
+	[scene addChild: layer];
+	
+	// return the scene
+	return scene;
+}
+
+-(id) init
+{
+	if( (self=[super init])) {
+		
+		// enable events
+		
+		self.touchEnabled = YES;
+		self.accelerometerEnabled = YES;
+		CGSize s = [CCDirector sharedDirector].winSize;
+		
+        
+		// init physics
+		[self initPhysics];
+        
+        //set up background
+        CGSize winSize = [CCDirector sharedDirector].winSize;
+        bool is5 = (winSize.height == 568) || (winSize.width == 568);
+        CGFloat centerOffsetX = is5 ? ((568-480)/2) : 0;
+        self.position = ccp(centerOffsetX,0);
+       
+        if(is5)
+        {
+            // load the extended background from iPhone5
+            [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"background-568h@2x.plist"];
+        }
+        else
+        {
+            // load the standard background
+            [[CCSpriteFrameCache sharedSpriteFrameCache] addSpriteFramesWithFile:@"background.plist"];
+        }
+		background = [CCSprite spriteWithSpriteFrameName:@"hardwood-wallpaper.jpg"];
+        [self addChild:background z:0];
+        background.anchorPoint = ccp(0,0);
+        background.position = ccp(-centerOffsetX,0);
+		//Set up sprite
+		// Use batch node. Faster
+		CCSpriteBatchNode *blockBatch = [CCSpriteBatchNode batchNodeWithFile:@"blocks.png" capacity:100];
+        spriteTexture_ = [blockBatch texture];
+		[self addChild:blockBatch z:1 tag:kTagParentNode];
+		[self addNewSpriteAtPosition:ccp(s.width/2, s.height/2)];
+		[self scheduleUpdate];
+	}
+	return self;
+}
+
+-(void) dealloc
+{
+	delete world;
+	world = NULL;
+	
+	delete m_debugDraw;
+	m_debugDraw = NULL;
+	
+	[super dealloc];
+}	
+
+
+-(void) initPhysics
+{
+	
+	CGSize s = [[CCDirector sharedDirector] winSize];
+	
+	b2Vec2 gravity;
+	gravity.Set(0.0f, -10.0f);
+	world = new b2World(gravity);
+	
+	
+	// Do we want to let bodies sleep?
+	world->SetAllowSleeping(true);
+	
+	world->SetContinuousPhysics(true);
+	
+	m_debugDraw = new GLESDebugDraw( PTM_RATIO );
+	world->SetDebugDraw(m_debugDraw);
+    //contact listener
+    _contactListener = new MyContactListener();
+    world->SetContactListener(_contactListener);
+	
+	uint32 flags = 0;
+	flags += b2Draw::e_shapeBit;
+	//		flags += b2Draw::e_jointBit;
+	//		flags += b2Draw::e_aabbBit;
+	//		flags += b2Draw::e_pairBit;
+	//		flags += b2Draw::e_centerOfMassBit;
+	m_debugDraw->SetFlags(flags);		
+	
+	
+	// Define the ground body.
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0, 0); // bottom-left corner
+	
+	// Call the body factory which allocates memory for the ground body
+	// from a pool and creates the ground box shape (also from a pool).
+	// The body is also added to the world.
+	b2Body* groundBody = world->CreateBody(&groundBodyDef);
+	
+	// Define the ground box shape.
+	b2EdgeShape groundBox;		
+	
+	// bottom
+	
+	groundBox.Set(b2Vec2(0,0), b2Vec2(s.width/PTM_RATIO,0));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// top
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// left
+	groundBox.Set(b2Vec2(0,s.height/PTM_RATIO), b2Vec2(0,0));
+	groundBody->CreateFixture(&groundBox,0);
+	
+	// right
+	groundBox.Set(b2Vec2(s.width/PTM_RATIO,s.height/PTM_RATIO), b2Vec2(s.width/PTM_RATIO,0));
+	groundBody->CreateFixture(&groundBox,0);
+}
+
+
+-(void) addNewSpriteAtPosition:(CGPoint)p
+{
+	// Define the dynamic body.
+	//Set up a 1m squared box in the physics world
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(p.x/PTM_RATIO, p.y/PTM_RATIO);
+	b2Body *body = world->CreateBody(&bodyDef);
+	
+	// Define another box shape for our dynamic body.
+	b2PolygonShape dynamicBox;
+	dynamicBox.SetAsBox(.5f, .5f);//These are mid points for our 1m box
+	
+	// Define the dynamic body fixture.
+	b2FixtureDef fixtureDef;
+	fixtureDef.shape = &dynamicBox;	
+	fixtureDef.density = 1.0f;
+	fixtureDef.friction = 0.3f;
+	body->CreateFixture(&fixtureDef);
+	
+
+	CCNode *parent = [self getChildByTag:kTagParentNode];
+	
+	//We have a 64x64 sprite sheet with 4 different 32x32 images.  The following code is
+	//just randomly picking one of the images
+	int idx = (CCRANDOM_0_1() > .5 ? 0:1);
+	int idy = (CCRANDOM_0_1() > .5 ? 0:1);
+	CCPhysicsSprite *sprite = [CCPhysicsSprite spriteWithTexture:spriteTexture_ rect:CGRectMake(32 * idx,32 * idy,32,32)];
+	[parent addChild:sprite];
+	
+	[sprite setPTMRatio:PTM_RATIO];
+	[sprite setB2Body:body];
+	[sprite setPosition: ccp( p.x, p.y)];
+
+}
+
+-(void) update: (ccTime) dt
+{
+	//It is recommended that a fixed time step is used with Box2D for stability
+	//of the simulation, however, we are using a variable time step here.
+	//You need to make an informed choice, the following URL is useful
+	//http://gafferongames.com/game-physics/fix-your-timestep/
+	
+	int32 velocityIterations = 8;
+	int32 positionIterations = 1;
+	
+	// Instruct the world to perform a single step of simulation. It is
+	// generally best to keep the time step and iterations fixed.
+	world->Step(dt, velocityIterations, positionIterations);
+	
+    std::vector<b2Body *>toDestroy;
+    std::vector<MyContact>::iterator pos;
+    for(pos = _contactListener->_contacts.begin();
+        pos != _contactListener->_contacts.end(); ++pos) {
+        MyContact contact = *pos;
+        
+        b2Body *bodyA = contact.fixtureA->GetBody();
+        b2Body *bodyB = contact.fixtureB->GetBody();
+        if (bodyA->GetUserData() != NULL && bodyB->GetUserData() != NULL) {
+            CCSprite *spriteA = (CCSprite *) bodyA->GetUserData();
+            CCSprite *spriteB = (CCSprite *) bodyB->GetUserData();
+            
+            if (spriteA.tag == 1 && spriteB.tag == 2) {
+                toDestroy.push_back(bodyA);
+            } else if (spriteA.tag == 2 && spriteB.tag == 1) {
+                toDestroy.push_back(bodyB);
+            }
+        }
+    }
+    
+    std::vector<b2Body *>::iterator pos2;
+    for(pos2 = toDestroy.begin(); pos2 != toDestroy.end(); ++pos2) {
+        b2Body *body = *pos2;
+        if (body->GetUserData() != NULL) {
+            CCSprite *sprite = (CCSprite *) body->GetUserData();
+            [_spriteSheet removeChild:sprite cleanup:YES];
+        }
+        _world->DestroyBody(body);
+    }
+    
+    if (toDestroy.size() > 0) {
+        [[SimpleAudioEngine sharedEngine] playEffect:@"hahaha.caf"];   
+    }
+}
+
+- (void)ccTouchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+	//Add a new body/atlas sprite at the touched location
+	for( UITouch *touch in touches ) {
+		CGPoint location = [touch locationInView: [touch view]];
+		location = [[CCDirector sharedDirector] convertToGL: location];
+		[self addNewSpriteAtPosition: location];
+	}
+}
+
+
+@end
---
+++ box_gravity/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  box_gravity
+//
+//  Created by Nick Martin on 11/11/13.
+//  Copyright Nick Martin 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
